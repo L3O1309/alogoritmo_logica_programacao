303242c70399b593d2324a38501c9983265b4eb0
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-08-18 09:37:48 -0300

    Update tirarcarta.py

--- Aula 3/tirarcarta.py
+++ Aula 3/tirarcarta.py
@@ -1,7 +1,17 @@
 ## Quem pode dirigir
-idade = float(input("Digite sua idade: "))
+idadep1 = float(input("Digite a idade da primeira pessoa: "))
+idadep2 = float(input("Digite a idade da segunda pessoa: "))
+idadep3 = float(input("Digite a idade da terceira pessoa: "))
 
-if idade >= 18: 
-  print("Pode tirar a carteira de motorista e dirigir!")
+if idadep1 >= 18:
+  print("Primeira idade pode votar!")
 else: 
-  print("Não pode dirigir!")
+  print("Primeira idade não pode votar!")
+if idadep2 >= 18:
+  print("Segunda idade pode votar!")
+else: 
+  print("Segunda idade não pode votar!")
+if idadep3 >= 18:
+  print("Terceira idade pode votar!")
+else: 
+  print("Terceira idade não pode votar!")
