ceaeb6414c206f4dd18af606c793d272acbc7c0d
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-08-24 10:44:46 -0300

    Update promocao.py

--- Aula 4/promocao.py
+++ Aula 4/promocao.py
@@ -3,7 +3,7 @@ promovido = input("Ganhou a promoção ? (digite sim ou não) ")
 salario = float(input("Digite seu salário: "))
 promocao = salario * 0.15
 salarionovo = salario + promocao
-if promovido == "Sim" or promovido == "sim" or promovido == "SIM":
+if promovido.lower() == "sim":
   print("Parabéns pela promoção, seu salário agora é: ", salarionovo)
 else:
   print("Sinto muito, continue se esforçando!!")
