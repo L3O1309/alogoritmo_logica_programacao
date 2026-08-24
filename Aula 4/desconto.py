425caba7f10811616120c09087fac7d3d14f1c70
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-08-24 10:45:57 -0300

    Simplify discount input check to case insensitive

--- Aula 4/desconto.py
+++ Aula 4/desconto.py
@@ -3,7 +3,7 @@ desconto = input("Digite sim se for pagar a vista: ")
 valorcompra = float(input("Digite o valor da compra: "))
 valordesconto = valorcompra * 0.10
 compracomdesconto = valorcompra + valordesconto
-if desconto == "Sim" or desconto == "sim" or desconto == "SIM":
+if desconto.lower() == "sim":
   print("O valor da compra com o desconto é: ", compracomdesconto)
 else: 
   print("Valor integral: ", valorcompra)
