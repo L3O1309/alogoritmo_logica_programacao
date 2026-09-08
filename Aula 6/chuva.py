1b02e25989fbd6489f9b714bb6ff210cecbfb764
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-09-08 08:05:18 -0300

    Create chuva.py

---
+++ Aula 6/chuva.py
@@ -0,0 +1,7 @@
+## Está chovendo ?
+chuva = input("Está chovendo agora ?(sim/não) ").lower().strip()
+
+if chuva == "sim" or chuva == "yes":
+  print("Está chovendo, leve um guarda-chuva!")
+else:
+  print("Não está chovendo, guarde o guarda-chuva.")
