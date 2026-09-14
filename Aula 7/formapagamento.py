425aadd3ad54fb0029277d50be6ee77449656352
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-09-14 10:24:56 -0300

    Update formapagamento.py

--- Aula 7/formapagamento.py
+++ Aula 7/formapagamento.py
@@ -1,14 +1,18 @@
 ## Opções de forma de pagamento
 lista = ["1. À Vista", "2. Crédito", "3. Parcelado 2x sem juros", "4. Parcelado 3x com juros"]
+def processar_pagamento(escolha):
+  match escolha:
+    case 1:
+      print("Sua opção escolhida foi: ", lista[0])
+    case 2:
+      print("Sua opção escolhida foi: ", lista[1])
+    case 3:
+      print("Sua opção escolhida foi: ", lista[2])
+    case 4:
+      print("Sua opção escolhida foi: ", lista[3])
+    case _: 
+      print("ERRO!")
+
 print("Suas opções de compras são: ", lista)
-escolha = int(input("Escolha uma forma de pagamento de acordo com o número antes da forma (1, 2, 3, 4): "))
-if escolha == 1:
-  print("Forma de pagamento:", lista[0])
-elif escolha == 2:
-  print("Forma de pagamento:", lista[1])
-elif escolha == 3:
-  print("Forma de pagamento:", lista[2])
-elif escolha == 4:
-  print("Forma de pagamento:", lista[3])
-else:
-  print("ERRO!")
+opcao_escolhida = int(input("Escolha uma forma de pagamento de acordo com o número antes da forma (1, 2, 3, 4): "))
+processar_pagamento(opcao_escolhida)
