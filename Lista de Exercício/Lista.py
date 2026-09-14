bf5479484f3e575b9bf83ebdfd597d16f6fd4012
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-09-14 11:24:15 -0300

    Update Lista.py

--- Lista de Exercício/Lista.py
+++ Lista de Exercício/Lista.py
@@ -334,23 +334,28 @@ def ex26():
 def ex27():
   ## Desconto progressivo
   ## < 100 10% entre 101 e 500 15% maior que 500 é 20%
-  valor = int(input("Digite o valor da compra: "))
-
-  if valor <= 100:
-    desconto1 = valor * 0.1
-    vd1 = valor - desconto1
-    print("Deconto de 10% ", vd1)
-  elif valor >= 101 and valor <= 500:
-    desconto2 = valor * 0.15
-    vd2 = valor - desconto2
-    print("Desconto de 15% ", vd2)
-  elif valor > 500:
-    desconto3 = valor * 0.2
-    vd3 = valor - desconto3
-    print("Desconto de 20% ", vd3)
-  else:
-    print("ERRO!")
-
+  def main():
+    print("Desconto progressivo: < 100 10% entre 101 e 500 15% maior que 500 é 20%")
+    preco = float(input("Digite o valor da compra: "))
+    preco_final = desconto_progressivo(preco)
+    print(f"Valor final da compra: ", preco_final)
+  
+  def desconto_progressivo(valor):
+    if valor <= 100:
+      desconto1 = valor * 0.1
+      vd1 = valor - desconto1
+      return vd1
+    elif valor >= 101 and valor <= 500:
+      desconto2 = valor * 0.15
+      vd2 = valor - desconto2
+      return vd2
+    elif valor > 500:
+      desconto3 = valor * 0.2
+      vd3 = valor - desconto3
+      return vd3
+    else:
+      print("ERRO! VOCÊ NÃO OBTEVE DESCONTO.")
+  main()
 def ex28():
   ## Opções de forma de pagamento
   lista = ["1. À Vista", "2. Crédito", "3. Parcelado 2x sem juros", "4. Parcelado 3x com juros"]
