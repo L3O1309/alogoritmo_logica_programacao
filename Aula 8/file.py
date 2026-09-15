7316f1ffe14ed4e39faf9406ee3251b5d30d59dd
Leonardo dos Santos Silva <211995506+L3O1309@users.noreply.github.com>
2026-09-15 08:32:31 -0300

    Update file.py

--- Aula 8/file.py
+++ Aula 8/file.py
@@ -1,1 +1,18 @@
-
+"""Como não fizemos nada além da correção
+   , vou fazer um código nada haver aqui"""
+def helloworld():
+  print("Hello World!")
+def olaestudante():
+  print("Olá Estudante!")
+def semanaprovas():
+  print("Semana de provas logo aí")
+decida = int(input("Digite um número de 1-3 para exibir uma mensagem no terminal: "))
+match decida:
+  case 1:
+    helloworld()
+  case 2:
+    olaestudante()
+  case 3:
+    semanaprovas()
+  case _:
+    print("Ocorreu um erro, tente novamente!")
